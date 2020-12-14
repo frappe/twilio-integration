189713db6a1606ac30b947114ba7857140247e0a
leela <leela.vadlamudi@gmail.com>
2020-12-14 16:52:17 +0530

    fix: Allow only One `voice call settings` doc per user
    Added validation to make sure that users always have one `voice call
    settings` record per user.

--- twilio_integration/public/js/voice_call_settings.js
+++ twilio_integration/public/js/voice_call_settings.js
@@ -1,12 +1,15 @@
 
 frappe.ui.form.on('Voice Call Settings', {
 	onload: function(frm) {
+		if (!frappe.user_roles.includes('System Manager')){
+			frm.set_value('user', frappe.session.user)
+		}
+	},
+	refresh: function(frm) {
 		frappe.call({
 			method: "twilio_integration.twilio_integration.api.get_twilio_phone_numbers",
 			callback: function(resp) {
 				if (resp.message.length) {
-					//FIXME: I am messy
-					resp.message.push('')
 					frm.set_df_property('twilio_number', 'options', resp.message);
 					frm.refresh_field('twilio_number');
 				}
@@ -16,6 +19,15 @@ frappe.ui.form.on('Voice Call Settings', {
 						indicator:'red'
 					}, 6);
 				}
+			}
+		});
+	},
+	user: function(frm) {
+		frappe.db.exists('Voice Call Settings', frm.doc.user).then( exists => {
+			if (exists) {
+				var doc_url = `/desk#Form/${frm.doc.doctype}/${frm.doc.user}`;
+				var link_html = `<a href="${doc_url}">here</a>`;
+				frappe.msgprint(__('Voice call settings already exists for this user. You can edit them {0}', [link_html]));
 			}
 		});
 	}
